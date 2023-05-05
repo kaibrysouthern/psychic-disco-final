2ed25c1e8281e41480eb924919fe072902ea3fff
kaibrysouthern <122912482+kaibrysouthern@users.noreply.github.com>
2023-05-05 16:47:30 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,29 @@
 $(function(){
-    $('#console-box').append('<br/>console is ready...');
-    
-    $('button').click(function(){
-      $('#console-box').prepend('<br>class:'+$(this).attr("class")+'|value:'+$(this).text()+'.');
-  ; 
-    });
-    
+  $('#console-box').append('<br/>console is ready...');
+  
+  $('button').click(function(){
+    $('#console-box').prepend('<br>class:'+$(this).attr("class")+'|value:'+$(this).text()+'.');
+; 
   });
-  $("#formSubmit").on("click", (e) => {
-    e.preventDefault();
-    console.log("clicked the button");
-    $("#result").html("<b>The button is pressed and form is submitted.</b>");
-    
-// Submit the form
-    $("form").submit();
+  
+});
 
-  });
-  //Refresh
 function refreshPage() {
-    $("#row").html("");
-    var getValue = document.getElementById("loadData");
-    if (getValue.value != "") {
-      getValue.value = "";
-    }
+  document.getElementById("year").value = "";
+  document.getElementById("carYear").textContent = "";
+
+}
+
+function submitForm() {
+  const yearInput = document.getElementById("year");
+  const year = yearInput.value;
+
+  if (year < 1800 || year > 2025) {
+    yearInput.setCustomValidity("Year must be between 1800 and 2025");
+    yearInput.reportValidity();
+  } else {
+    document.getElementById("carYear").textContent = `The year of the car is ${year}.`;
+    yearInput.setCustomValidity("");
   }
+}
+
